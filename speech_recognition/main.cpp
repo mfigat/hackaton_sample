7fae9998052296c11c4696cda455723ec69de84f
Maksym Figat <maksym.figat44@gmail.com>
2016-05-19 17:52:19 +0200

    Add speech recognition

--- speech_recognition/main.cpp
+++ speech_recognition/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char * argv[]) {
     comm.play_audio(path);
 
     // Recognize word from a captured sound
-    rapp::cloud::speech_detection_sphinx4("/home/nao/recordings/microphones/rapp_email.ogg",
+    std::string recognized_word=rapp::cloud::speech_detection_sphinx4("/home/nao/recordings/microphones/rapp_email.ogg",
         "en",
         {"monday", "tuesday"},
         {"monday", "tuesday"},
@@ -36,6 +36,7 @@ int main(int argc, char * argv[]) {
         "9090",
         true
     );
+    comm.text_to_speech(recognized_word);
 
     return 0;
 }
