e50f61dd23211489764d1a3e8433542565038b81
Maksym Figat <maksym.figat44@gmail.com>
2016-05-20 15:45:27 +0200

    update

--- speech_recognition/main.cpp
+++ speech_recognition/main.cpp
@@ -13,36 +13,24 @@ int main(int argc, char * argv[]) {
     rapp::robot::info info(argc, argv);
     rapp::robot::communication comm(argc, argv);
     
-    comm.text_to_speech("Recording the sound");
     // set recording time
     int recording_time=3;
+
+    comm.text_to_speech("What is your name?");
     // record audio
     std::string path = comm.capture_audio(recording_time);
     // Sound was recored
-    comm.text_to_speech("Sound was recorded");
+    //comm.text_to_speech("Sound was recorded");
     // Play captured sound
-    comm.text_to_speech("Playing the captured sound");
+    //comm.text_to_speech("Playing the captured sound");
     // play recorded audio
-    comm.play_audio(path);
-
-      // Recognize word from a captured sound
-    rapp::cloud::speech_detection_sphinx4("/home/nao/ws_rapp_applications_nao/src/rapp-applications-nao/nao/data/mail/sounds/rapp_email.ogg",
-        "en",
-        {"monday", "tuesday"},
-        {"monday", "tuesday"},
-        {},
-        "nao_ogg",
-        "rapp",
-        "roman",
-        "9090",
-        true
-    );
+    //comm.play_audio(path);
 
     // Recognize word from a captured sound
-    std::vector<std::string> words =rapp::cloud::speech_detection_sphinx4("/home/nao/ws_rapp_applications_nao/src/rapp-applications-nao/nao/data/mail/sounds/rapp_email.ogg",
+    std::vector<std::string> words = rapp::cloud::speech_detection_sphinx4("/home/nao/ws_rapp_applications_nao/src/rapp-applications-nao/nao/data/mail/sounds/rapp_email.ogg",
         "en",
-        {"monday", "tuesday"},
-        {"monday", "tuesday"},
+        {"elephant", "banana", "marmot"},
+        {"elephant", "banana", "marmot"},
         {},
         "nao_ogg",
         "rapp",
@@ -50,11 +38,29 @@ int main(int argc, char * argv[]) {
         "9090",
         true
     );
-
-    comm.text_to_speech("Recognized word");
+    comm.text_to_speech("Your name is");
     for (unsigned i=0; i<words.size(); i++)
         comm.text_to_speech(words.at(i));
 
+    comm.text_to_speech("What color do you like?");
+    // record audio
+    std::string path = comm.capture_audio(recording_time);
+    // Recognize word from a captured sound
+    std::vector<std::string> words =rapp::cloud::speech_detection_sphinx4("/home/nao/ws_rapp_applications_nao/src/rapp-applications-nao/nao/data/mail/sounds/rapp_email.ogg",
+        "en",
+        {"red", "blue", "pink", "green"},//{"monday", "tuesday"},
+        {"red", "blue", "pink", "green"},
+        {},
+        "nao_ogg",
+        "rapp",
+        "roman",
+        "9090",
+        true
+    );
+
+    comm.text_to_speech("You like");
+    for (unsigned i=0; i<words.size(); i++)
+        comm.text_to_speech(words.at(i));
 
     return 0;
 }
